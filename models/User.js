1d0e64d246620041d39c80300165c2deac68504c
Breanne <BreMarotta@gmail.com>
2023-01-25 18:23:42 -0500

    create User model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true, 
+        maxLength: 20
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: [
+            'Male',
+            'Female',
+            'Non-Binary'
+        ]
+    },
+    email: {
+        type: String,
+        required: false,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+    
+}, {
+    timestamps: true
+})
+
+module.exports =mongoose.model('User', UserSchema);
